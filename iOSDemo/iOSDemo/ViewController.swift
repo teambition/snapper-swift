ec86220520a9332bfa0b125c6c3e5040f5d202ff
HarriesChen <info@mrchenhao.com>
2015-12-23 16:15:28 +0800

    add gitignore

--- iOSDemo/iOSDemo/ViewController.swift
+++ iOSDemo/iOSDemo/ViewController.swift
@@ -11,7 +11,7 @@ import Snapper
 
 class ViewController: UIViewController {
 
-    let snapper = SnapperClient(socketURL: "snapper.project.bi/websocket", options: [.Log(false), .ConnectParams(["token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiI1NWM4MTdmOGU3MTVmYTk5MmExOTNlOTkiLCJleHAiOjE0NTA5MzYwODd9.Y9gk3d3YTE3loyUxWSxiVJzHjhe12bn5O5qGsuJ89JE"])])
+    let snapper = SnapperClient(socketURL: "snapper.project.bi/websocket", options: [.Log(true), .ConnectParams(["token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiI1NWM4MTdmOGU3MTVmYTk5MmExOTNlOTkiLCJleHAiOjE0NTA5MzYwODd9.Y9gk3d3YTE3loyUxWSxiVJzHjhe12bn5O5qGsuJ89JE"])])
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
         snapper.on("connect") { (data) -> Void in
             print("connect")
             print(self.snapper.status)
+        }
+        
+        snapper.on("error") { (data) -> Void in
+            
+        }
+        
+        snapper.on("reconnect") { (data) -> Void in
+            
         }
         
         snapper.message { (message: SnapperMessage) -> Void in
